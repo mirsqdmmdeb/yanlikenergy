d65dfbb8120816f87bd562eebab6b073c6985b01
Mirsad Mirbey <mirbeymiray1@gmail.com>
2025-10-21 21:40:13 +0300

    Update state.js

--- state.js
+++ state.js
@@ -1,16 +1,13 @@
 export class YanlikState {
   constructor() {
     this.memory = JSON.parse(localStorage.getItem('yanlik_memory') || '[]');
+    this.energy = JSON.parse(localStorage.getItem('yanlik_energy') || 'false');
   }
-
-  remember(msg) {
-    this.memory.push(msg);
-    if (this.memory.length > 1000) this.memory.shift();
+  remember(line) {
+    this.memory.push(line);
+    if (this.memory.length > 2000) this.memory.shift();
     localStorage.setItem('yanlik_memory', JSON.stringify(this.memory));
   }
-
-  reset() {
-    this.memory = [];
-    localStorage.removeItem('yanlik_memory');
-  }
+  isEnergy() { return !!this.energy; }
+  setEnergy(v) { this.energy = !!v; localStorage.setItem('yanlik_energy', JSON.stringify(v)); }
 }
